23a994be444907a87ea00d24ccf6b2df5f6db4bc
Izdihar Zuhdi <izdiharzuhdi@MacBook-Pro.local>
2019-12-16 05:38:32 +0800

    with slides

--- app/src/components/section/slides.js
+++ app/src/components/section/slides.js
@@ -36,7 +36,8 @@ const References = React.createClass({
             <section id='slides'>
                 <div className='text-container'>
                     <div className='row'>
-                            <Carousel
+                            <div>{this.props.content.pdf}</div>
+                            {/* <Carousel
                             // autoplay={carouselConfig.autoplay}
                             // decorators={carouselConfig.decorators}
                             // wrapAround={carouselConfig.wrapAround}
@@ -48,7 +49,7 @@ const References = React.createClass({
                                         <Entry key={index} entry={entry} />
                                     );
                                 })}
-                            </Carousel>
+                            </Carousel> */}
                     </div>
                 </div>
             </section>
